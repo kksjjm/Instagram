d022407c9b68c6ef7246fc40722712793bb88eaf
짱짱맨 <60203153+kksjjm@users.noreply.github.com>
2023-09-26 14:06:38 +0000

    add model feature and migration

--- houses/models.py
+++ houses/models.py
@@ -6,7 +6,9 @@ class House(models.Model):
     """Model definition of Houses"""
 
     name = models.CharField(max_length=140)
-    price = models.PositiveIntegerField()
+    price_per_night = models.PositiveIntegerField()
     description = models.TextField()
     address = models.CharField(max_length=140)
+    max_guest_number = models.PositiveSmallIntegerField(default=1)
+    pet_allowed = models.BooleanField(default=True)
 
